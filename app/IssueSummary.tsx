037d444e1678e4290abbb34e8a763df990d1d454
francis <FrancisMingomba>
2024-12-24 16:28:18 +0000

    Working on home page

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -1,7 +1,9 @@
+
 import { Status } from '@prisma/client';
-import { Card, Flex, Text } from '@radix-ui/themes';
+import { Card,Text, Flex } from '@radix-ui/themes'
 import Link from 'next/link';
-import React from 'react';
+import React from 'react'
+
 
 interface Props {
   open: number;
@@ -9,38 +11,34 @@ interface Props {
   closed: number;
 }
 
-const IssueSummary = ({ open, inProgress, closed }: Props) => {
-  const containers: {
-    label: string;
-    value: number;
-    status: Status;
-  }[] = [
-    { label: 'Open Issues', value: open, status: 'OPEN' },
-    {
-      label: 'In-progress Issues',
-      value: inProgress,
-      status: 'IN_PROGRESS',
-    },
-    { label: 'Closed Issues', value: closed, status: 'CLOSED' },
-  ];
+const IssueSummary = ({open, inProgress, closed}: Props) => {
 
+    const containers:{
+      label: string;
+      value: number;
+      status: Status
+
+    }[] = [
+      {label: 'Open Issue', value: open, status: 'OPEN'},
+      {label: 'In-Progress Issue', value: inProgress, status: 'IN_PROGRESS'},
+      {label: 'Closed Issue', value: closed, status: 'CLOSED'},
+    ];
+  
   return (
-    <Flex gap="4">
-      {containers.map((container) => (
+   <Flex>
+      {containers.map(container => (
         <Card key={container.label}>
-          <Flex direction="column" gap="1">
-            <Link
-              className='text-sm font-medium'
-              href={`/issues/list?status=${container.status}`}
+          <Flex direction="column">
+            <Link href={`/issues/list?status=${container.status}`}
             >
               {container.label}
-            </Link>
-            <Text size="5" className='font-bold'>{container.value}</Text>
+              </Link>
+              <Text>{container.value}</Text>
           </Flex>
         </Card>
       ))}
-    </Flex>
-  );
-};
+   </Flex>
+  )
+}
 
-export default IssueSummary;
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import prisma from '@/prisma/client';
 import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
@@ -19,21 +18,21 @@ export default async function Home() {
 
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
-      <Flex direction="column" gap="5">
-        <IssueSummary
-          open={open}
-          inProgress={inProgress}
-          closed={closed}
-        />
-        <IssueChart
-          open={open}
-          inProgress={inProgress}
-          closed={closed}
-        />
-      </Flex>
-      <LatestIssues />
-    </Grid>
-  );
+    <Flex direction="column" gap="5">
+      <IssueSummary
+        open={open}
+        inProgress={inProgress}
+        closed={closed}
+      />
+      <IssueChart
+        open={open}
+        inProgress={inProgress}
+        closed={closed}
+      />
+    </Flex>
+    <LatestIssues />
+  </Grid>
+);
 }
 
 export const dynamic = 'force-dynamic'; 
